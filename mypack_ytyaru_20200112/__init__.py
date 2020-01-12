bae62fd375fc9341998e99019762644eca3b3c39
ytyaru <ytyaru@outlook.jp>
2020-01-12 16:19:56 +0900

    Forgot to fix the version.

--- mypack_ytyaru_20200112/__init__.py
+++ mypack_ytyaru_20200112/__init__.py
@@ -3,7 +3,7 @@
 from mypack_ytyaru_20200112 import *
 
 __copyright__    = 'Copyright (C) 2020 ytyaru'
-__version__      = '0.0.3'
+__version__      = '0.0.4'
 #__license__      = 'GNU Affero General Public License Version 3 (AGPL-3.0)'
 __license__      = 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
 __author__       = 'ytyaru'
